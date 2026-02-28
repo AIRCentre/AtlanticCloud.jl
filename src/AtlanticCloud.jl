f5cee0f35da7e3fea646e7289d19d4c5b5580b7a
jpinelo <j.pinelo@mac.com>
2026-02-28 16:15:53 +0000

    Issue 4: HTTP client and raw request function

--- src/AtlanticCloud.jl
+++ src/AtlanticCloud.jl
@@ -1,5 +1,7 @@
 module AtlanticCloud
 
+using HTTP
+
 const DEFAULT_BASE_URL = "https://services.aircentre.org"
 
 struct AtlanticCloudError <: Exception
@@ -24,12 +26,18 @@ struct AtlanticCloudClient
             throw(AtlanticCloudError(
                 "No API key provided. Pass api_key= directly or set the " *
                 "ATLANTICCLOUD_API_KEY environment variable. " *
-                "Register at https://services.aircentre.org/access/"
+                "Register at https://services.aircentre.org/access/account"
             ))
         end
 
         new(base_url, resolved_key)
     end
+end
+
+function _get(client::AtlanticCloudClient, path::String)
+    url = client.base_url * path
+    response = HTTP.get(url, ["X-API-Key" => client.api_key])
+    return String(response.body)
 end
 
 export AtlanticCloudClient, AtlanticCloudError
